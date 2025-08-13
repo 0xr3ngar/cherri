c3bd9483b4061cac97d9defde8e56e986008c931
bnn16 <bogdan.nikolov4@outlook.com>
2025-08-13 14:04:57 +0300

    chore: remove cli.ts for index.ts

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env bun
+import { Command } from "commander";
+import { cherriCommand } from "./command/cherri";
+
+const program = new Command();
+
+program
+    .name("cherri")
+    .description("Automated cherry-picking for PRs marked with 🍒")
+    .version("0.1.0");
+
+program
+    .requiredOption("-o, --owner <owner>", "GitHub repository owner")
+    .requiredOption("-r, --repo <repo>", "GitHub repository")
+    .requiredOption("-t, --target <branch>", "Target branch")
+    .requiredOption("-f, --from <commit>", "Starting commit SHA")
+    .option(
+        "-s, --since <months>",
+        "Number of months to look back for PRs",
+        "1",
+    )
+    .option("-i, --icon <icon>", "Custom icon for the logo", "🍒")
+    .action(cherriCommand);
+
+program.parse();
