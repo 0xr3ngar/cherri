598c4456c6ae29d30a3656d89d1a8543d5cbc75d
bnn16 <bogdan.nikolov4@outlook.com>
2025-08-28 13:58:05 +0300

    chore: bump monor version

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const program = new Command();
 program
     .name("cherri")
     .description("Automated cherry-picking for PRs marked with 🍒")
-    .version("1.0.5");
+    .version("1.0.6");
 
 program
     .requiredOption("-o, --owner <owner>", "GitHub repository owner")
